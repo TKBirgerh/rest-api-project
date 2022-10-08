10d2184dca75df2b8d1ef2388e956ed07c385ac2
TKBirgerh <45202082+TKBirgerh@users.noreply.github.com>
2022-10-08 13:58:45 +0200

    Added test endpoints
    Added two endpoints for fetching store and fetching a specific item in the store.

--- app.py
+++ app.py
@@ -18,6 +18,23 @@ stores = [
 def get_stores():
     return {"stores": stores}
 
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>/item")
+def get_items_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404
+
+
 @app.get("/")
 def index():
     return render_template("home.html")
