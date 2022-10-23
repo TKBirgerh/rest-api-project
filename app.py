101f77babc23ba48ef109af9b4dfe7e07cd980c2
TKBirgerh <45202082+TKBirgerh@users.noreply.github.com>
2022-10-23 16:15:25 +0200

    implemented swagger docs with flask smorest

--- app.py
+++ app.py
@@ -1,84 +1,22 @@
-from flask import Flask, render_template, request
-from db import items, stores
-from flask_smorest import abort
-import uuid
+from flask import Flask
+from flask_smorest import Api
+
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
 
 app = Flask(__name__)
 
-# Stores
-@app.get("/store")
-def get_stores():
-    return {"stores": list(stores.values())}
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Stores REST API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = (
+    "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+)
 
+api = Api(app)
 
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        abort(404, message="Store not found")
-
-
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()
-    if "name" not in store_data:
-        abort(
-            400,
-            message="Bad request. Ensure 'name' is included in the JSON payload."
-        )
-    for store in stores.values():
-        if store_data["name"] == store["name"]:
-            abort(400, message=f"Store already exists.")
-    store_id = uuid.uuid4().hex
-    store = {**store_data, "id": store_id}
-    stores[store_id] = store
-    return store, 201
-
-
-# Homepage
-@app.get("/")
-def index():
-    return render_template("home.html")
-
-
-# Items
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        abort(404, message="Store not found")
-    except Exception as e:
-        abort(500, message=e)
-
-
-@app.post("/item")
-def create_item():
-    item_data = request.get_json()
-    if (
-        "price" not in item_data
-        or "store_id" not in item_data
-        or "name" not in item_data
-    ):
-        abort(
-            400,
-            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload."
-        )
-    for item in item.values():
-        if(
-            item_data["name"] == item["store_id"]
-        ):
-            abort(400, message=f"Item already exists.")
-    if item_data["store_id"] not in stores:
-        abort (404, message="Store not found")
-    
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-
-    return item, 201
+api.register_blueprint(StoreBlueprint)
+api.register_blueprint(ItemBlueprint)
